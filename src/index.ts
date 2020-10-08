e47afc01a8d518bb60dd6c4341b33451b9213054
Ricardo Ledan <ricoledan@gmail.com>
2020-10-08 00:46:20 -0400

    build(transformxml): update transformer to create new object with needed values

--- src/index.ts
+++ src/index.ts
@@ -29,14 +29,27 @@ const search = (name: string, page = 1) => {
     })
 }
 
-// Parses XML response and transforms it into JSON
 const transformXml = (xml: any) => {
   xml2js.parseString(xml, (err, output) => {
-    return console.log(
-      JSON.stringify(output.GoodreadsResponse.search[0].results, null, 1)
-    )
+    const jsonOutput = output.GoodreadsResponse.search[0].results[0].work
+
+    function getValues(json: any): Book {
+      let i: number
+      const obj: any = []
+      for (i = 0; i < json.length; i++) {
+        obj.push({
+          title: jsonOutput[i].best_book[0].title[0],
+          author: jsonOutput[i].best_book[0].author[0].name[0],
+          image_url: jsonOutput[i].best_book[0].image_url[0],
+          publication_year: jsonOutput[i].original_publication_year[0]['_'],
+          avg_rating: jsonOutput[i].average_rating[0],
+        })
+      }
+      return obj
+    }
+    return console.log(getValues(jsonOutput))
   })
 }
 
-console.log(search('Harry Potter', 1))
+search('Octavia E. Butler')
 export { search }
--- src/types.ts
+++ src/types.ts
@@ -3,7 +3,7 @@ interface Book {
   author: string
   image_url: string
   publication_year: string
-  average_rating: string
+  avg_rating: string
 }
 
 export { Book }
